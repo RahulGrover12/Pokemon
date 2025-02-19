6970960e6cad3531e725f424cc2dfca83a733993
Rahul Grover <groverrahul47@gmail.com>
2025-02-20 02:55:15 +0530

    final commit

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import PokemonCards from "./PokemonCards";
+
+const Pokemon = () => {
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=31";
+
+  const [pokemonData, setPokemonData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [searchPokemon, setSearchPokemon] = useState("");
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      //   console.log(data);
+      const detailedData = data.results.map(async (poke) => {
+        // console.log(poke.url);
+        const res = await fetch(poke.url);
+        const data = await res.json();
+        return data;
+      });
+      const fullData = await Promise.all(detailedData);
+      setPokemonData(fullData);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const filteredData = pokemonData.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchPokemon.toLowerCase())
+  );
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading....</h1>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>{error}</h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <section className="container">
+        <header>
+          <h1>Lets Catch Pokemon</h1>
+        </header>
+
+        <div className="pokemon-search">
+          <input
+            type="text"
+            placeholder="Search for Pokemon"
+            value={searchPokemon}
+            onChange={(e) => setSearchPokemon(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <ul className="cards">
+            {filteredData.map((pokemon) => {
+              return <PokemonCards key={pokemon.id} pokemon={pokemon} />;
+            })}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Pokemon;
---
+++ src/components/PokemonCards.jsx
@@ -0,0 +1,53 @@
+const PokemonCards = ({ pokemon }) => {
+  return (
+    <>
+      <li className="pokemon-card">
+        <figure>
+          <img
+            src={pokemon.sprites.other.dream_world.front_default}
+            alt={pokemon.name}
+            className="pokemon-image"
+          />
+        </figure>
+        <h1 className="pokemon-name">{pokemon.name}</h1>
+        <div className="pokemon-info pokemon-highlight">
+          <p>{pokemon.types.map((type) => type.type.name).join(", ")}</p>
+        </div>
+
+        <div className="grid-three-cols">
+          <p className="pokemon-info">
+            <span>Height:</span> {pokemon.height}
+          </p>
+          <p className="pokemon-info">
+            <span>Weight:</span> {pokemon.weight}
+          </p>
+          <p className="pokemon-info">
+            <span>Speed:</span> {pokemon.stats[0].base_stat}
+          </p>
+        </div>
+
+        <div className="grid-three-cols">
+          <div className="pokemon-info">
+            <p>{pokemon.base_experience}</p>
+            <span>Experience:</span>
+          </div>
+          <div className="pokemon-info">
+            <p>{pokemon.stats[1].base_stat}</p>
+            <span>Attack:</span>
+          </div>
+          <div className="pokemon-info">
+            <p>
+              {pokemon.abilities
+                .map((abilitys) => abilitys.ability.name)
+                .slice(0, 1)
+                .join(", ")}
+            </p>
+            <span>Abilities:</span>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
+
+export default PokemonCards;
